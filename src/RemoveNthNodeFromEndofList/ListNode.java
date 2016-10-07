4aeca3847a99dd11e4b5558090a75bf6260d2d2c
shanxu100 <shanxu100@163.com>
2016-10-07 23:44:16 +0800

    Algorithem

---
+++ src/RemoveNthNodeFromEndofList/ListNode.java
@@ -0,0 +1,13 @@
+package RemoveNthNodeFromEndofList;
+
+/**
+ * Created by lianglitu on 16/10/7.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/RemoveNthNodeFromEndofList/Solution.java
@@ -0,0 +1,23 @@
+package RemoveNthNodeFromEndofList;
+
+/**
+ * Created by lianglitu on 16/10/7.
+ */
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode tail=head;
+        while (n>1)
+        {
+            tail=tail.next;
+        }
+
+        while (tail.next==null)
+        {
+            tail=tail.next;
+            head=head.next;
+        }
+
+        return head;
+
+    }
+}
