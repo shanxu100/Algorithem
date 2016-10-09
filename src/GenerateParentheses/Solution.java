f77b04d7302332fddaa308b08a70a401bd7c1685
shanxu100 <shanxu100@163.com>
2016-10-09 20:12:18 +0800

    算法

---
+++ src/GenerateParentheses/Solution.java
@@ -0,0 +1,17 @@
+package GenerateParentheses;
+
+import java.util.List;
+
+/**
+ * Created by guan on 10/9/16.
+ */
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
+}
