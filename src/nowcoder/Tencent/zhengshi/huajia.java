926999025b9e558793b7f537a0977030ebd04404
GuanSX <shanxu100@163.com>
2018-04-05 19:23:25 +0800

    practice

--- src/nowcoder/Tencent/zhengshi/huajia.java
+++ src/nowcoder/Tencent/zhengshi/huajia.java
@@ -8,25 +8,96 @@ import java.util.Scanner;
  */
 public class huajia {
 
+    public static int result;
+    public static int N, M;
+    public static String[][] matrix;
+
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
 
-        int N, M;
-        String[][] matrix;
         while (in.hasNextLine()) {
-            String[] tmp=in.nextLine().split(" ");
+            String[] tmp = in.nextLine().split(" ");
             N = Integer.valueOf(tmp[0]);
             M = Integer.valueOf(tmp[1]);
             matrix = new String[N][];
             for (int i = 0; i < N; i++) {
                 matrix[i] = in.nextLine().split(" ");
             }
-            System.out.println(3);
+            result = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (matrix[i][j].equals("B")) {
+                        fixB(i, j);
+                    } else if (matrix[i][j].equals("Y")) {
+                        fixY(i, j);
+                    } else if (matrix[i][j].equals("G")) {
+                        fixG(i, j);
+                    }
+                }
+            }
 
+            System.out.println(result);
 
         }
 
+    }
+
+    public static void fixG(int gx, int gy) {
+        fixB(gx, gy);
+        fixY(gx, gy);
+    }
+
+    public static void fixY(int bx, int by) {
+        result++;
+        int x = bx, y = by;
+
+        while (x < N && x >= 0 && y >= 0 && y < M) {
+            if (matrix[x][y].equals("G")) {
+                matrix[x][y] = "B";
+            } else {
+                matrix[x][y] = "X";
+            }
+            x++;
+            y++;
+        }
+        x = bx;
+        y = by;
+        while (x < N && x >= 0 && y >= 0 && y < M) {
+            if (matrix[x][y].equals("G")) {
+                matrix[x][y] = "B";
+            } else {
+                matrix[x][y] = "X";
+            }
+            x--;
+            y--;
+        }
+    }
+
+    public static void fixB(int bx, int by) {
+        result++;
+        int x = bx, y = by;
+        while (x < N && x >= 0 && y >= 0 && y < M) {
+            if (matrix[x][y].equals("G")) {
+                matrix[x][y] = "Y";
+            } else {
+                matrix[x][y] = "X";
+            }
+            x++;
+            y--;
+        }
+        x = bx;
+        y = by;
+        while (x < N && x >= 0 && y >= 0 && y < M) {
+            if (matrix[x][y].equals("G")) {
+                matrix[x][y] = "Y";
+            } else {
+                matrix[x][y] = "X";
+            }
+            x--;
+            y++;
+        }
     }
 
 }
