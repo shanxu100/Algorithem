cddaab7a029b40fec14bc4e5f54db4b84f5ccb33
GuanSX <shanxu100@163.com>
2018-09-08 15:58:33 +0800

    practice

---
+++ src/netease/school/Q1.java
@@ -0,0 +1,39 @@
+package netease.school;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author Guan
+ * @date Created on 2018/9/8
+ */
+public class Q1 {
+
+    private static int[] a=new int[3];
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            a[0] = in.nextInt();
+            a[1] = in.nextInt();
+            a[2] = in.nextInt();
+            f();
+
+        }
+
+
+    }
+
+    public static void f() {
+        Arrays.sort(a);
+        int result=0;
+        result+=Math.abs(a[0]-a[1]);
+        result+=Math.abs(a[1]-a[2]);
+        System.out.println(result);
+
+    }
+
+
+}
