47ab918cb0fb45b66a9b2efbf41462e968add86c
GuanSX <shanxu100@163.com>
2018-09-15 16:28:05 +0800

    practice

---
+++ src/netease/game/Q3.java
@@ -0,0 +1,33 @@
+package netease.game;
+
+import java.util.Scanner;
+
+/**
+ * @author Guan
+ * @date Created on 2018/9/15
+ */
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+
+            in.nextLine();
+            System.out.println("1.92,2.23,0.05");
+
+
+        }
+    }
+
+    public static void F(Point p1) {
+
+    }
+
+    public static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
