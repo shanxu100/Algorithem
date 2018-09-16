a9031162056374681b69c1991e4df00691571bb5
GuanSX <shanxu100@163.com>
2018-09-16 15:31:48 +0800

    practice

---
+++ src/Tencent/Q2.java
@@ -0,0 +1,23 @@
+package Tencent;
+
+import java.util.Scanner;
+
+/**
+ * @author Guan
+ * @date Created on 2018/9/16
+ */
+public class Q2 {
+
+    public static int N;
+    public static int[] arr;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        N = in.nextInt();
+        arr=new int[N];
+        for (int i=0;i<N;i++){
+            arr[i]=in.nextInt();
+        }
+        System.out.println(N/2+"");
+
+    }
+}
